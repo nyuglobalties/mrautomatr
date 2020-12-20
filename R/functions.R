6f28ec3b69f1ba8f623aa30eb4257c6607bc604c
Michael Wu <wuzezhen33@gmail.com>
2020-12-20 16:24:21 -0500

    bug fixes

--- R/functions.R
+++ R/functions.R
@@ -221,7 +221,7 @@ get.R2 <- function(model, path){
 # check if mplus is in version 8
 check.mplus.version.invariance <- function(model){
   
-  if(as.numeric(model$Mplus.version) >= 8){ # (Column `SRMR` doesn't exist. because the models output from Mplus 7 don't have SRMR)
+  if(as.numeric(model$Mplus.version) > 8){ # (Column `SRMR` doesn't exist. because the models output from Mplus 7 don't have SRMR)
     model.s <- select(model, Parameters:RMSEA_Estimate, SRMR, Filename)} else{
       warning("Upgrade Mplus to Version 8 to include SRMR as a model fit index. Mplus Version 7 reports WRMR instead.")
       model.s <- select(model, Parameters:RMSEA_Estimate, WRMR, Filename)    
